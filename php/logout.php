2cdb050dc5d25bba36eb5e3d4c44bc6dac372e26
Nghia Le <ntl61415@uga.edu>
2017-02-13 18:22:20 -0500

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -2,4 +2,13 @@
 session_start();
 session_destroy();
 
+
+date_default_timezone_set('AMERICA/NEW_YORK');
+$oneMonth = 60 * 60 * 24 * 30 + time();
+$date_and_time_temp = new DateTime();
+$date_and_time = date_format($date_and_time_temp, 'm/d/Y H:i:s');
+//setcookie('visit', $date_and_time, $oneMonth);
+setcookie('visit', $date_and_time, $oneMonth);
+
+
 header("Location: ../start.php");
