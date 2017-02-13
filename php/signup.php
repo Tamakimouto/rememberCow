08859a3c494d53c7ba2c75c9bf9b65691da25120
Terrence Butler <tbbutle@uga.edu>
2017-02-13 18:52:51 -0500

    Update Signeup.php
    Re-Add Reg Expression

--- php/signup.php
+++ php/signup.php
@@ -6,14 +6,26 @@ include 'database.php';
 $un = $_POST['username'];
 $pw = $_POST['password'];
 
-$sql = "INSERT INTO users (username, password) VALUES ('$un', '$pw')";
-$success = $conn->query($sql);
+/*
+ *
+ * Compares username and Password to Regular Expressions
+ * Password: ^(?=.*[a-z])([0-9a-z]){3,8}$
+ * Username: ^(?=.{6,12}$)([0-9])(.*)([^0-9A-Za-z]+)$
+ */
+if(preg_match("~^(?=.{6,12}$)([0-9])(.*)([^0-9A-Za-z]+)$~", $pw) && (preg_match("~^(?=.*[a-z])([0-9a-z]){3,8}$~", $un))){
+    $sql = "INSERT INTO users (username, password) VALUES ('$un', '$pw')";
+    $success = $conn->query($sql);
 
-$_SESSION['user'] = $un;
+    $_SESSION['user'] = $un;
 
-$result = array("Location" => "php/list.php");
+    $result = array("Location" => "php/list.php");
 
-header("Content-Type: application/json");
-echo json_encode($result);
+    header("Content-Type: application/json");
+    echo json_encode($result);
+}
+else{
+
+    header("Location: ../start.php");
+}
 
 ?>
